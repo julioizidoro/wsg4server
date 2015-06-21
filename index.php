21806097ba07694366670bd21c58960b86754a38
André Luiz Banki <andre.banki@gmail.com>
2015-06-20 23:10:50 -0300

    Ajustes de formatação

--- index.php
+++ index.php
@@ -6,20 +6,20 @@ $app = new \Slim\Slim();
 $app->response()->header('Content-Type', 'application/json;charset=utf-8');
 $app->response()->header('Access-Control-Allow-Origin', '*');
 
-$app->get('/corridas', 'GetCorridas');
-$app->post('/corridas', 'AddCorrida');
+$app->get('/corridas', 'getCorridas');
+$app->post('/corridas', 'addCorrida');
 
-$app->get('/corridas/:id', 'GetCorridaById');
-$app->put('/corridas/:id', 'UpdateCorridaById');
-$app->delete('/corridas/:id', 'DeleteCorridaById');
+$app->get('/corridas/:id', 'getCorrida');
+$app->put('/corridas/:id', 'updateCorrida');
+$app->delete('/corridas/:id', 'deleteCorrida');
 
-$app->get('/corredores', 'GetCorredores');
-$app->post('/corredores', 'AddCorredor');
+$app->get('/corredores', 'getCorredores');
+$app->post('/corredores', 'addCorredor');
 
-$app->get('/corridas/:id/corredores', 'GetCorredoresNaCorrida');
-$app->post('/corridas/:id/corredores', 'InscreveCorredorNaCorrida');
+$app->get('/corridas/:id/corredores', 'getCorredoresNaCorrida');
+$app->post('/corridas/:id/corredores', 'inscreveCorredorNaCorrida');
 
-$app->put('/corridas/:idCorrida/corredores/:idCorredor', 'UpdateInscricao');
+$app->put('/corridas/:idCorrida/corredores/:idCorredor', 'updateInscricao');
 
 $app->run();
 
@@ -30,7 +30,13 @@ function getConn()
 	   			));
 }
 
-function GetCorridas() {
+function getRequestContents() 
+{
+	$request = \Slim\Slim::getInstance()->request();
+	return json_decode($request->getBody());
+}
+
+function getCorridas() {
 	$stmt = getConn()->query("SELECT * FROM Corrida");
 	$corridas = $stmt->fetchAll(PDO::FETCH_OBJ);
 
@@ -38,10 +44,9 @@ function GetCorridas() {
 	echo '{"corridas":'.json_encode($corridas)."}";
 }
 
-function AddCorrida()
+function addCorrida()
 {
-	$request = \Slim\Slim::getInstance()->request();
-	$corrida = json_decode($request->getBody());
+	$corrida = getRequestContents();
 	$sql = "INSERT INTO corridas (nome,preco,dataInclusao,idCategoria) values (:nome,:preco,:dataInclusao,:idCategoria) "; //todo
 	$conn = getConn();
 	$stmt = $conn->prepare($sql);
@@ -56,7 +61,7 @@ function AddCorrida()
 	echo json_encode($corrida);
 }
 
-function GetCorridaById($id)
+function getCorrida($id)
 {
 	$conn = getConn();
 	$sql = "SELECT * FROM corrida WHERE idcorrida=:id";
@@ -74,10 +79,9 @@ function GetCorridaById($id)
 	}
 }
 
-function UpdateCorridaById($id)
+function updateCorrida($id)
 {
-	$request = \Slim\Slim::getInstance()->request();
-	$corrida = json_decode($request->getBody());
+	$corrida = getRequestContents();
 	$sql = "UPDATE corridas SET nome=:nome,preco=:preco,dataInclusao=:dataInclusao,idCategoria=:idCategoria WHERE   idcorrida=:id"; // todo
 	$conn = getConn();
 	$stmt = $conn->prepare($sql);
@@ -92,7 +96,7 @@ function UpdateCorridaById($id)
 	echo json_encode($corrida);
 }
 
-function DeleteCorridaById($id)
+function deleteCorrida($id)
 {
 	$sql = "DELETE FROM corridas WHERE idcorrida=:id";
 	$conn = getConn();
@@ -104,18 +108,17 @@ function DeleteCorridaById($id)
 	echo "{'message':'Produto apagado'}";
 }
 
-function GetCorredores() {
-	$stmt = getConn()->query("SELECT * FROM corredores");
+function getCorredores() {
+	$stmt = getConn()->query("SELECT * FROM corredor");
 	$corredores = $stmt->fetchAll(PDO::FETCH_OBJ);
 
 	http_response_code(200);
 	echo '{"corredores":'.json_encode($corredores)."}";
 }
 
-function AddCorredor()
+function addCorredor()
 {
-	$request = \Slim\Slim::getInstance()->request();
-	$corredor = json_decode($request->getBody());
+	$corredor = getRequestContents();
 	$sql = "INSERT INTO corredores (nome,preco,dataInclusao,idCategoria) values (:nome,:preco,:dataInclusao,:idCategoria) "; //todo
 	$conn = getConn();
 	$stmt = $conn->prepare($sql);
@@ -130,7 +133,7 @@ function AddCorredor()
 	echo json_encode($corredor);
 }
 
-function GetCorredoresNaCorrida($id)
+function getCorredoresNaCorrida($id)
 {
 	$conn = getConn();
 	$sql = "SELECT c.* FROM inscricoes i INNER JOIN corredores c ON (i.corredor_idcorredor = c.idcorredor) WHERE corrida_idcorrida=:id"; 
@@ -143,10 +146,9 @@ function GetCorredoresNaCorrida($id)
 	echo '{"corredores":'.json_encode($corredores)."}";
 }
 
-function InscreveCorredorNaCorrida($id)
+function inscreveCorredorNaCorrida($id)
 {
-	$request = \Slim\Slim::getInstance()->request();
-	$corredor = json_decode($request->getBody());
+	$corredor = getRequestContents();
 	$sql = "INSERT INTO inscricoes (corrida_idcorrida, corredor_idcorredor, statuspagamento) values (:id_corrida,:id_corredor, false) "; 
 	$conn = getConn();
 	$stmt = $conn->prepare($sql);
@@ -156,10 +158,9 @@ function InscreveCorredorNaCorrida($id)
 	http_response_code(201);
 }
 
-function UpdateInscricao($idCorrida, $idCorredor)
+function updateInscricao($idCorrida, $idCorredor)
 {
-	$request = \Slim\Slim::getInstance()->request();
-	$inscricao = json_decode($request->getBody());
+	$inscricao = getRequestContents();
 	$sql = "UPDATE inscricoes SET statuspagamento=:status_pgto,posicao=:posicao,tempo=:tempo ".
 	               "WHERE corrida_idcorrida = :id_corrida AND corredor_idcorredor = :id_corredor";
 
