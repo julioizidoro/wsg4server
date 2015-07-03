20032e28f2cb4dd1dbdd1dd367ffc21d6c568db8
alex <alex@teclan.com.br>
2015-07-03 09:25:31 -0300

    Retirada das chamadas específicas de OPTIONS
    Retirada das chamadas específicas de OPTIONS

--- index.php
+++ index.php
@@ -18,36 +18,28 @@ $app->options('/(:name+)', function() use ($app) {
 include 'corridas.php';
 $app->get('/corridas', 'getCorridas');
 $app->post('/corridas', 'addCorrida');
-$app->options('/corridas', function() {});
 $app->get('/corridas/ultimas', 'getUltimasCorridas');
 $app->get('/corridas/proximas', 'getProximasCorridas');
 $app->get('/corridas/abertas', 'getCorridasAbertas');
 $app->get('/corridas/:id', 'getCorrida');
 $app->put('/corridas/:id', 'updateCorrida');
-$app->options('/corridas/:id', function() {});
 $app->delete('/corridas/:id', 'deleteCorrida');
 
 include 'corredores.php';
 $app->get('/corredores', 'getCorredores');
 $app->post('/corredores', 'addCorredor');
-$app->options('/corredores', function() {});
 $app->get('/corredores/ativos', 'getCorredoresAtivos');
 $app->get('/corredores/:id', 'getCorredor');
 $app->put('/corredores/:id', 'updateCorredor');
-$app->options('/corredores/:id', function() {});
 $app->delete('/corredores/:id', 'deleteCorredor');
 
 include 'inscricoes.php';
 $app->get('/corridas/:id/corredores', 'getCorredoresNaCorrida');
 $app->get('/corredores/:id/corridas', 'getCorridasDoCorredor');
 $app->post('/corridas/:idCorrida/corredores/:idCorredor', 'inscreveCorredorNaCorrida');
-$app->options('/corridas/:idCorrida/corredores/:idCorredor', function() {});
 $app->post('/corredores/:idCorredor/corridas/:idCorrida', 'inscreveCorredorNaCorrida');
-$app->options('/corredores/:idCorredor/corridas/:idCorrida', function() {});
 $app->put('/corridas/:idCorrida/corredores/:idCorredor', 'updateInscricao');
-$app->options('/corridas/:idCorrida/corredores/:idCorredor', function() {});
 $app->put('/corredores/:idCorredor/corridas/:idCorrida', 'updateInscricao');
-$app->options('/corredores/:idCorredor/corridas/:idCorrida', function() {});
 $app->get('/corridas/:idCorrida/corredores/:idCorredor', 'getInscricaoCorrida');
 $app->get('/corredores/:idCorredor/corridas/:idCorrida', 'getInscricaoCorredor');
 $app->delete('/corridas/:idCorrida/corredores/:idCorredor', 'deleteInscricaoCorrida');
